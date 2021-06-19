0504542aba7e8f129ffcd8ad8180e58ef2f03bb8
sharathkaarthick <kaarthick.sk@gmail.com>
2021-06-19 17:00:47 +0530

    validated template driven forms

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -7,15 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 model:any={}
+resume:any
 submitted = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectFile(event:any) {
+    this.resume = event.target.files[0];
+
+    if (this.resume.type == 'application/pdf') {
+
+    } else {
+      alert("File type should be .pdf")
+      return;
+    }
+
+  }
+
   onSubmit(){
     this.submitted=true
     console.log(this.model)
+this.selectFile(event)
+console.log(this.resume)
+
   }
 
 }
